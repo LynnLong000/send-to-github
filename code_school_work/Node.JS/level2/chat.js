4cd5172a14f325ddca4ae7bc97eaa40e5045c6cf
Lynn Long Jr <lynnlong000@gmail.com>
2017-10-03 23:25:10 +0000

    2.2 chat emitter 2.2

---
+++ code_school_work/Node.JS/level2/chat.js
@@ -0,0 +1,10 @@
+// chat emitter 2.2
+
+var events = require('events');
+var EventEmitter = events.EventEmitter;
+
+var chat = new EventEmitter();
+
+chat.on('message', function(message){
+  console.log(message);
+});
