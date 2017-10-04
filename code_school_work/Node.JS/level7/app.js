76204cbb81ebb00b0dd322e20be25b706e4c35c3
Lynn Long Jr <lynnlong000@gmail.com>
2017-10-04 09:33:11 +0000

    do it your self level 7.3 updated app.js

--- code_school_work/Node.JS/level7/app.js
+++ code_school_work/Node.JS/level7/app.js
@@ -1,6 +1,11 @@
 // 7.2 simple redis commmands
+// 7.3 get a key 
 
 var redis = require('redis');
 var client = redis.createClient();
 
-client.set("name","Lynn");
+//client.set("name","Lynn");
+
+client.get("question",function(error,data){
+  console.log(data);
+});
