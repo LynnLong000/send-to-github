b091604b01d8db7db4186c0d61947b57e72d0f95
Lynn Long Jr <lynnlong000@gmail.com>
2017-10-04 07:42:14 +0000

    do it your self 5.7  app3.js

--- code_school_work/Node.JS/level5/app3.js
+++ code_school_work/Node.JS/level5/app3.js
@@ -1,6 +1,6 @@
-// 5.5 url building   updaing for 5.6 doing the requests
+// 5.5 url building   updaing for 5.6 doing the requests // 5.7 update  express server
 var url = require('url');
-
+var express = require('express'); // u5.7 updated
 var options = {
   // add URL options here
   protocol:'http://search.twitter.com/search.json?q=codeschool',
@@ -12,9 +12,18 @@ var options = {
 };
 
 var searchURL = url.format(options);
-//console.log(searchURL);
+var app = express(); // Create server here
+
+app.get('/',function(req,res){
+  request(searchURL).pipe(res);
+});
+
+app.listen(8080);
+/*
+console.log(searchURL);
 var request = require('request');
 
 request(searchURL,function(err,response,body){
   console.log(body);
 });
+*/
