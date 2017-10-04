21b6361f19a84b85a44744e024d336f1fed44b66
Lynn Long Jr <lynnlong000@gmail.com>
2017-10-04 04:29:25 +0000

    3.3 File piping updating file app.js

--- code_school_work/Node.JS/level3/app.js
+++ code_school_work/Node.JS/level3/app.js
@@ -1,11 +1,16 @@
-// 3.2 file Read Stream
+// 3.3 file Piping 
 
 var fs = require('fs');
-var file = fs.createReadStream("fruits.txt");
 
-file.on('readable',function(){
-  var chunk = null;
-  while (null !== (chunk = file.read())){
+var file = fs.createReadStream('fruits.txt');
+
+file.pipe(process.stdout);
+
+  /*
+file.on('readable', function() {
+  var chunk;
+  while(null !== (chunk = file.read())){
     console.log(chunk.toString());
   }
 });
+ */
