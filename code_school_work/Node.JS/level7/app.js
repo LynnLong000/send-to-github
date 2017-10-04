5727bbac8f4a00ee48254d6cedefe4b6932501f6
Lynn Long Jr <lynnlong000@gmail.com>
2017-10-04 09:41:30 +0000

    do it your self level 7.5 updated app.js

--- code_school_work/Node.JS/level7/app.js
+++ code_school_work/Node.JS/level7/app.js
@@ -4,16 +4,16 @@
 
 var redis = require('redis');
 var client = redis.createClient();
-
+/*
 var question1 = "Where is the dog?";
 var question2 = "Where is the cat?";
 
-/* client.set("name","Lynn");
+ client.set("name","Lynn");
 
 client.get("question",function(error,data){
   console.log(data);
 });
-*/
+
 
 client.lpush("questions",question1,function(error,value){
   console.log(value);
@@ -21,4 +21,9 @@ client.lpush("questions",question1,function(error,value){
 
 client.lpush("questions",question2,function(error,value){
   console.log(value);
+});
+*/
+
+client.lrange("questions", 0, -1, function(err, messages){
+ console.log(messages);
 });
