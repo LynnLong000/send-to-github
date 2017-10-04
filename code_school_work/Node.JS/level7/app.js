19b997b1e5f170592d8bd6a7a36f182ddec9390b
Lynn Long Jr <lynnlong000@gmail.com>
2017-10-04 09:54:00 +0000

    do it your self level 7.6 - 7.8 updated app.js

--- code_school_work/Node.JS/level7/app.js
+++ code_school_work/Node.JS/level7/app.js
@@ -2,9 +2,48 @@
 // 7.3 get a key 
 // 7.4 working with list 1
 // 7.5 working with list 2 
+// 7.6 Persisting 
+// 7.7 emitting stored Questions 
+// 7.8 Limiting Questions stored
+
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var socket = require('socket.io');
+var io = socket.listen(server);
 
 var redis = require('redis');
-var client = redis.createClient();
+var redisClient = redis.createClient();
+
+io.sockets.on('connection', function(client) {
+    redisClient.lrange('questions',0,-1,function(err,question){
+        question.forEach(function(question){
+            client.emit('question',question);
+        });   
+  });
+    
+    client.on('answer', function(question, answer) {
+        client.broadcast.emit('answer', question, answer);
+    });
+
+    client.on('question', function(question) {
+        if(!client.question_asked) {
+            client.question_asked = true;
+            client.broadcast.emit('question', question);
+            // add the question to the list here
+            redisClient.lpush("questions",question, function(err,response){
+                redisClient.ltrim('questions',0,19);
+            });
+        }
+    });
+});
+
+
+
+
+
+
+
 /*
 var question1 = "Where is the dog?";
 var question2 = "Where is the cat?";
@@ -23,8 +62,9 @@ client.lpush("questions",question1,function(error,value){
 client.lpush("questions",question2,function(error,value){
   console.log(value);
 });
-*/
+
 
 client.lrange("questions", 0, -1, function(err, messages){
  console.log(messages);
 });
+*/
