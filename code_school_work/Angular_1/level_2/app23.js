e20e82fd19b4cb7601e7f86314543aaf73e94786
Lynn Long Jr <lynnlong000@gmail.com>
2017-10-06 02:38:30 +0000

    level2  angular1 finished

---
+++ code_school_work/Angular_1/level_2/app23.js
@@ -0,0 +1,15 @@
+// 2.3 Display the first image 
+
+(function() {
+  var app = angular.module('gemStore', []);
+
+  app.controller('StoreController', function(){
+    this.products = gems;
+  });
+
+  var gems = [
+    { name: 'Azurite', price: 110.50 },
+    { name: 'Bloodstone', price: 22.90 },
+    { name: 'Zircon', price: 1100 },
+  ];
+})();
