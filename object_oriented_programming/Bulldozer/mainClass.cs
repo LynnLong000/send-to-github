0ed704d3d6a813207e1262b0074a36e82b636ea3
Lynn Long Jr <lynnlong000@gmail.com>
2018-01-13 17:00:56 +0000

    adding more deatails to the file

--- object_oriented_programming/Bulldozer/mainClass.cs
+++ object_oriented_programming/Bulldozer/mainClass.cs
@@ -6,7 +6,26 @@ namespace Bulldozer
     {
         public static void Main (string[] args)
         {
+            var dozer1 =  new BullDozer ("Yellow" , "Mini ");
             
+            var dozer2 =  new BullDozer ("red" , "Large");
+            
+            Console.WriteLine ( "Bulldozer 1 is: " + dozer1.color);
+            
+            Console.WriteLine ( "Bulldozer 2 is: " + dozer2.color);
         }
+    }
+    
+    public class BullDozer 
+    {
+        //constructor 
+        public BullDozer (string _color, string _size)
+        {
+            this.color = _color ; 
+            this.size = _size
+        }
+        
+        public string color{ get; set;} 
+        public string size {get; set; } 
     }
 }
